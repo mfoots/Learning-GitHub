7022ae404c6abdb9d1d63e592516f7255473fd3b
mfoots <mfoots@gmail.com>
2022-01-01 06:09:13 -0600

    Initial Commit

---
+++ Learning-GitHub/Learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitHubApp.swift
+//  Learning-GitHub
+//
+//  Created by Michael Foots on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
